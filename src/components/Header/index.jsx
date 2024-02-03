a6c544350c9ebff60e37a04c354f28d724a45254
nataliaclem <nataliaclementinosilva@gamil.com>
2024-02-02 21:00:32 -0300

    feat: add header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import css from "./styles.module.css"
+
+const Header = () => {
+    return (
+        <header className={css.container}>
+            <h1>Dashboard</h1>
+        </header>
+    )
+}
+
+export default Header
